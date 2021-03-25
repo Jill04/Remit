2c0202183037966618d397429eb1079f4169d143
Jill04 <69854931+Jill04@users.noreply.github.com>
2021-03-25 15:32:57 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,13 @@ var Remit = artifacts.require("Remit.sol");
 var RemitStaking = artifacts.require("RemitStaking.sol");
 var RemitVaultFarmEth = artifacts.require("RemitVaultFarmEth.sol");
 var Mistic =  artifacts.require("Mistic.sol");
+var RemitPreSale = artifacts.require("RemitPreSale.sol");
+var accounts = web3.utils.getAccounts();
 module.exports = async function(deployer)
 {
 	 await deployer.deploy(Remit);
 	 await deployer.deploy(RemitStaking,Remit.address);
 	 await deployer.deploy(RemitVaultFarmEth);
 	 await deployer.deploy(Mistic);
+	 await deployer.deploy(RemitPreSale,Remt.address,accounts[9])
 };	
